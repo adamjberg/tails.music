808e6cc8367468e9c94f0a5ea9320fb8b19a5e20
Adam Berg <adam@xyzdigital.com>
2025-09-05 21:09:21 -0700

    Move to posts folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./Home.tsx";
 import { Blog } from "./Blog.tsx";
-import { MusicIndustryThoughts } from "./MusicIndustryThoughts.tsx";
+import { MusicIndustryThoughts } from "./posts/MusicIndustryThoughts.tsx";
 
 export function App() {
   return (
